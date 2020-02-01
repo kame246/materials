f1e2d11a9ba8d3a630452cd8719275c90fc87003
kame246 <58433923+kame246@users.noreply.github.com>
2020-02-01 20:53:51 +0100

    Add script for SQL
    Just to test executescript method

---
+++ script.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Planet (
+ id INTEGER PRIMARY KEY,
+ name TEXT NOT NULL,
+);
+
+INSERT INTO TABLE Planet (name) VALUES ('Merkury');
+INSERT INTO TABLE Planet (name) VALUES ('Wenus');
+INSERT INTO TABLE Planet (name) VALUES ('Ziemia');
+INSERT INTO TABLE Planet (name) VALUES ('Mars');
+INSERT INTO TABLE Planet (name) VALUES ('Jowisz');
+INSERT INTO TABLE Planet (name) VALUES ('Saturn');
+INSERT INTO TABLE Planet (name) VALUES ('Uran');
+INSERT INTO TABLE Planet (name) VALUES ('Neptun');
